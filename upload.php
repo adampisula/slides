d7e761b540cbe392a3117fd110756d1e27870501
Adam Pisula <adam.pisula@outlook.com>
2019-05-07 11:28:52 +0200

    Other file formats + some redirections

--- upload.php
+++ upload.php
@@ -1,7 +1,31 @@
 <?php
+
+    function redirect_post($url, array $data, array $headers = null) {
+        $params = array(
+            'http' => array(
+                'method' => 'POST',
+                'content' => http_build_query($data)
+            )
+        );
+        if (!is_null($headers)) {
+            $params['http']['header'] = '';
+            foreach ($headers as $k => $v) {
+                $params['http']['header'] .= "$k: $v\n";
+            }
+        }
+        $ctx = stream_context_create($params);
+        $fp = @fopen($url, 'rb', false, $ctx);
+        if ($fp) {
+            echo @stream_get_contents($fp);
+            die();
+        } else {
+            // Error
+            throw new Exception("Error loading '$url', $php_errormsg");
+        }
+    }
+
     $directory = "presentation/";
     $extensions = array("tar");
-    $base_url = "/slides";
 
     if(isset($_FILES["presentation_file"])) {
         $errors = array();
@@ -35,16 +59,30 @@
             try {
                 $phar = new PharData($directory.$id."/".$id.".".$file_ext);
                 $phar->extractTo($directory.$id);
-            } catch (Exception $e) {
-                echo "Whoops, we couldn't unpack your presentation. Here's your error: ".$e->getMessage()."<br>";
+            } catch (Exception $e) {                
+                $errors[] = "Whoops, we couldn't unpack your presentation. Here's your error: ".$e->getMessage()."<br>";
             }
 
-            if(!copy("src/index_html.template", $directory.$id."/index.html"))
-                echo "Oh man, it looks like we couldn't copy some template files :( That's lame<br>";
+            if(!copy("src/index_php.template", $directory.$id."/index.php"))
+                $errors[] = "Oh man, it looks like we couldn't copy some template files :( That's lame<br>";
 
             unlink($directory.$id."/".$id.".".$file_ext);
 
-            header("Location: ".$base_url."/presentation/".$id);
+            $files = array_diff(scandir($directory.$id."/slides"), array('..', '.'));
+
+            if(count($files) > 1)
+                $format = end(explode(".", $files[2]));
+
+            else
+                $errors[] = "It looks like your presentation's broken! ¯\_(ツ)_/¯";
+
+            echo $id;
+            header("Location: ".$directory.$id."?format=".$format);
+
+            if(!empty($errors)) {
+                rmdir($directory.$id);
+                echo "Look, we're really sorry, but you got these errors:<ul><li>".implode("</li><li>", $errors)."</li></ul>";    
+            }
         }
         
         else {
